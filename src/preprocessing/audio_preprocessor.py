f279519bfb7800739522f4e844caf8acb91f7688
Eren Ünal <ernunl719@gmail.com>
2026-04-09 01:42:16 +0300

    add normalize and resample functions, create preprocessing class and define processing function

--- src/preprocessing/audio_preprocessor.py
+++ src/preprocessing/audio_preprocessor.py
@@ -2,19 +2,36 @@ import numpy as np
 import librosa
 import noisereduce as nr
 
+def normalize(audio_arr):
+    peak = np.max(audio_arr)
+    return (audio_arr / peak)
+
+def resample16kHz(audio_arr, orig_sampling_rate):
+    return librosa.resample(audio_arr, orig_sampling_rate, 16000)
+
 class PreProcessingPipeline:
-    def __init__(self, audiofile, sr):
-        self.audiofile = audiofile
-        self.sr = sr
+    def __init__(self, audio_filepath, sampling_rate, prop_decrease):
+        self.audio_filepath = audio_filepath
+        self.sampling_rate = sampling_rate
+        self.prop_decrease = prop_decrease
     
-    def process(self, filepath):
-        audio = librosa.load(filepath,sr=self.sr)
+    def process(self):
+        voice, sampling_rate = librosa.load(self.audio_filepath, sr=None)
+        voice_trimmed, _ = librosa.effects.trim(voice)
+        voice_reduced_noise = nr.reduce_noise(voice_trimmed,sr=self.sampling_rate,prop_decrease=self.prop_decrease)
+        voice_normalized = normalize(voice_reduced_noise)
+        voice_resampled_16kHz = resample16kHz(voice_normalized, self.sampling_rate)
+        
+        metadata = {"Shape: " : voice_resampled_16kHz.shape,
+                    "Sampling Rate: " : "16kHz",
+                    "Duration: " : len(voice_resampled_16kHz)/16000,
+        }
 
-        audio_trim, idx = librosa.effects.trim(audio)
-        audio_trim_nr = nr.reduce_noise(audio_trim, prop_decrease=0.5)
-        audio_processed = None
+        return voice_resampled_16kHz, metadata
 
-        metadata = {"Shape" : None}
+
+
+        
 
 
         
